1038a55dacc5293a6d4e3bf8d3d0e10fbe4dca3c
vitorlosing1 <vitorluricii@hotmail.com>
2024-09-02 22:39:32 -0300

    initial-update

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Text, View } from "react-native";
+
+import { style } from "./styles";
+
+export default function Login() {
+  return (
+    <View style={style.container}>
+      <View></View>
+      <View></View>
+      <View></View>
+    </View>
+  );
+}
